97a1d6860eec5715d9d566b8767264a2c004749b
Prabhudatta Mishra <84654664+Prabhudatta3004@users.noreply.github.com>
2026-06-29 12:03:44 -0700

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lookup = {")":"(","}":"{","]":"["}
+        stack = []
+        for ch in s:
+            if ch not in lookup:
+                stack.append(ch)
+            else:
+                if not stack or stack.pop() != lookup[ch]:
+                    return False
+        return not stack
