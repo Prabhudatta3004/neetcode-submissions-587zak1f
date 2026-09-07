533d3d76145b70b5cd8352a63c098afa6d05749b
Prabhudatta Mishra <84654664+Prabhudatta3004@users.noreply.github.com>
2026-09-07 08:22:21 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        in_degree = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
+
+        for v,u in prerequisites:
+            graph[u].append(v)
+            in_degree[v] +=1
+        
+
+        queue = deque([i for i in range(numCourses) if in_degree[i]==0])
+
+        res = []
+
+        while queue:
+            node = queue.popleft()
+            res.append(node)
+
+            for nbr in graph[node]:
+                in_degree[nbr] -=1
+                if in_degree[nbr] == 0:
+                    queue.append(nbr)
+        
+        if len(res) == numCourses:
+            return True
+        else:
+            return False
